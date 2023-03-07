c1902630d838a0981d3eacebb145958a20b20fd0
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-03-07 15:10:02 -0600

    Tipo de datos
    Explicando a detalle cada tipo de datos que existen

---
+++ 05-tipo_de_datos.js
@@ -0,0 +1,21 @@
+'use strict'
+
+alert("hola");
+
+//Operadores
+var numero1 = 7;
+var numero2 = 12;
+var resultado = numero1 + numero2;
+
+alert("El resultado de la operacion es: " + resultado);
+
+//Tipos de datos
+var numero_entero = 44;
+var cadena_texto = "Hola que tal, esta es una prueba";
+var verdader_0_falso = false;
+var numero_falso = "33";
+
+console.log(typeof numero_entero);
+console.log(typeof cadena_texto);
+console.log(typeof verdader_0_falso);
+console.log(typeof numero_falso);
