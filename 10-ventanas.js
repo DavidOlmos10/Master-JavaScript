9e1901e46e7eceab69790b088cd0203aeb65acbd
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-07-20 13:02:51 -0600

    Ventanas
    Se agregaron ejemplos de los tipos de ventanas, por ejemplo, alertas de confirmacion y de ingreso de datos

---
+++ 10-ventanas.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//Alerta
+
+//alert("Esta es mi alerta !!");
+//alert("Hola mundooooo");
+
+//Alertas de Confirmacion
+
+ //var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+ //console.log(mi_resultado);
+
+ //Ingreso de ddatos
+ var res = prompt("¿Que edad tienes?", 18);
+ console.log(res);
