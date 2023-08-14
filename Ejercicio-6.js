f93e1a43d757cd7a052b112bcef03ad56aa31480
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-08-14 12:18:08 -0600

    Ejercicio 6
    Se realizo un ejemplo el ciclo while y la condicion if para saber si un numero es par o impar

---
+++ Ejercicio-6.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*
+Hacer un programa que diga si el numero es par o impar.
+1.Ventana prompt
+2.Si no es valido que nos pida de nuevo el numero
+*/
+
+var number = parseInt(prompt("Introduce un numero",0));
+
+while(isNaN(number)){
+    number = parseInt(prompt("Introduce un numero",0));
+}
+
+if(number % 2 == 0){
+    alert("Es numero par");
+}else{
+    alert("Es numero impar");
+}
+
