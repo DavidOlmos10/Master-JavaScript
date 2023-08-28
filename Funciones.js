be402e5e4dbae8ba516fddf592d28d1da98fb8fb
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-08-27 23:24:35 -0600

    Ejercicio 1 coon funciones
    Desarrollamos un ejercicio con una funcion

---
+++ Funciones.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+
+//Funciones
+//Es una agrupacion de instrucciones, las cuales tienen un nombre en concreto y tienen como tal una tarea
+// que se ejecutara cuando la mandes a llamar
+
+//Se define la funcion
+function calculadora(){
+   
+    //Conjunto de instrucciones 
+    console.log("Hola soy la calculadora");
+    console.log("si soy yo");
+   return "Hola, soy la calculadora";  // Con la palabara return regresa un valor 
+    
+}
+
+//Mandar a llamar la funcion
+
+calculadora();
+
+
+
