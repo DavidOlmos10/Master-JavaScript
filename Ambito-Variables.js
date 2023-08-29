10ae43834c6f7c65f1e737b9daf53cb9c5592513
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-08-28 23:16:47 -0600

    Ambitos de variables
    Se creo un nuevo fichero en el proyecto, se esta realizando diferentes ejemploa de variables y de converciones de numeros a string con la funcion toString

---
+++ Ambito-Variables.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function holaMundo(texto){
+
+    //Se crea una variable local llamada hola_mundo
+    var hola_mundo = "Texto dentro de una funcion";
+    console.log(texto);
+    console.log(numero.toString());
+    console.log(hola_mundo);
+}
+
+var numero = 12;
+var texto = "Hola mundo desde vscode";
+holaMundo(texto);
